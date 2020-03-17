7225573713b9f96cadf3d7fbb8a727b019288f6d
mickael ruzel <mickael.ruzel@gmail.com>
2020-03-17 11:14:49 +0100

    Initial Commit

---
+++ NewWarriorsQuest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NewWarriorsQuest
+//
+//  Created by mickael ruzel on 17/03/2020.
+//  Copyright © 2020 mickael ruzel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
